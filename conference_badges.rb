6ec41ee05bb980ef42710070eeb812c94d0957b3
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-26 21:17:31 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |speakers|
+    return "Hello, my name is ."
+    index += 1
+  end
+end
