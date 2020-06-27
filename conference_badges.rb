43a8871bc629697146440b4b9d20a07941b328c0
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-27 16:52:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,21 @@ end
 
 def batch_badge_creator(array)
   array.collect do |speakers|
-    return "Hello, my name is ."
-    index += 1
+    "Hello, my name is #{speakers}."
+  end
+end
+
+def assign_rooms(array)
+   array.collect.with_index do |speakers, index|
+     "Hello, #{speakers}! You'll be assigned to room #{index + 1}!"
+ end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
   end
 end
